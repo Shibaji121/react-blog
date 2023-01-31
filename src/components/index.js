3d33421ce30cb4b11d478f7f7a35e3361f229231
shiba_1234 <shibajisahu121@gmail.com>
2023-01-31 23:37:35 +0530

    router set up done

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import App from "./App";
+import Navbar from "./Navbar";
+import PostDetail from "./PostDetail";
+import Home from "./Home.";
+import CreatePost from "./CreatePost";
+
+
+export {
+    App,
+    Navbar,
+    Home,
+    CreatePost,
+    PostDetail
+}
