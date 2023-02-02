582388de40e52a69674d4648c4df8f0984e4cc3d
shiba_1234 <shibajisahu121@gmail.com>
2023-02-02 22:43:44 +0530

    set Home page details

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import {firestore} from '../firebase';
+
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firestore.collection('posts')
+    .get()
+    .then((snapshot) => {
+      const posts = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        };
+      });
+      setPosts(posts);
+    });
+  }, []);
+
     return (
-      <div className="">
-        Home
+      <div className="home">
+        <h1>Tech Blog</h1>
+        <div id="blog-by">Shibaji</div>
+        {posts.map((post, index) => (
+          <div className="post" key={`post-${index}`}>
+            <Link to={`/post/${post.id}`}>
+              <h3>{post.title}</h3>
+            </Link>
+            <p>{post.subtitle}</p>
+          </div>  
+        ))}
       </div>
     );
   }
