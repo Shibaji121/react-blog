de6c7c769a902ad4bfa847a474a2ffd54f3628f6
shiba_1234 <shibajisahu121@gmail.com>
2023-02-02 23:01:38 +0530

    post detail page implemented

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { firestore } from "../firebase";
+
 function PostDetail() {
+
+  const [post, setPost] = useState({});
+  const {postId} = useParams();
+
+  useEffect(() => {
+    firestore.collection('posts')
+    .doc(postId)
+    .get()
+    .then((snapshot) => {
+      setPost(snapshot.data());
+    });
+  },[]);
+
     return (
-      <div>
-        PostDetail
+      <div className="post-detail">
+        <h1>{post.title}</h1>
+        <p>{post.content}</p>
       </div>
     );
   }
